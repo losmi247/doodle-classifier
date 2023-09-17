a6c05f558287e1b7978b2a45607288576a5c2639
Milos Puric <milospuric856@gmail.com>
2023-09-17 11:45:26 +0200

    add list of nb feature classes that abstract base class will use

--- src/naive_bayes/classifier/features.py
+++ src/naive_bayes/classifier/features.py
@@ -1,11 +1,13 @@
 import numpy as np
 from abc import ABC, abstractmethod
 
+
 #
 # Astract base class for a feature used by the 
 # Naive Bayes classifier.
 #
-class NBFeature(ABC):
+
+class NBFeature(ABC):    
     # method that takes a list of 28x28 pixels images,
     # and returns a dictionary that maps different
     # features OF SAME TYPE to the number of times they
@@ -19,23 +21,33 @@ class NBFeature(ABC):
     # and returns a dictionary that maps different
     # features OF ALL POSSIBLE TYPES to the number of times they
     # appear in the set of images.
-    # After creating more features, update this method.
+    #
+    # all implemented feature types must implement the count_feature_in_images
+    # function, as indicated by the abstract method count_feature_in_images above.
     @staticmethod
     def count_features_in_images(images):
-        return PositionedPixel.count_feature_in_images(images)
+        feature_counts = {}
+        # go through all implemented feature types
+        for feature_class in NBFeature.all_feature_classes:
+            feature_counts.update(feature_class.count_feature_in_images(images))
+        return feature_counts
 
     # method to gather all features in a list, from all
-    # extensions of this abstract base class. To add more,
-    # simply extend the returned list.
+    # extensions of this abstract base class.
+    #
+    # all implemented feature types must have a all_features static field
+    # that contains a numpy array of all different features of that type.
     @staticmethod
     def get_all_features():
-        return PositionedPixel.all_features
+        return np.concatenate([feature_class.all_features for feature_class in NBFeature.all_feature_classes])
     
     # method to extract all existing features in a given image.
-    # After creating more features, update this method.
+    #
+    # all implemented feature types must implement the extract_features
+    # function to return a numpy array of features of that type from the image.
     @staticmethod
     def extract_features(image):
-        return PositionedPixel.extract_features(image)
+        return np.concatenate([feature_class.extract_features(image) for feature_class in NBFeature.all_feature_classes])
 
 
 #
@@ -73,7 +85,7 @@ class PositionedPixel(NBFeature):
         return PositionedPixel.all_features[PositionedPixel.encode_feature(x, y, value)]
     
     # method to extract all existing PositionedPixel features
-    # from the given iamge
+    # from the given image, in numpy array.
     @staticmethod
     def extract_features(image):
         features = np.empty(28*28, NBFeature)
@@ -81,11 +93,23 @@ class PositionedPixel(NBFeature):
             features[28*y+x] = PositionedPixel.get_feature(x, y, image[x, y])
         return features
 
-# static variables for the PositionedPixel class, i.e. 
-# a list of all possible features of this type
+
+#
+# Static variables for the PositionedPixel class
+#
+
+# a numpy array of all possible features of this type
 PositionedPixel.all_features = np.empty(28*28*256, NBFeature)
 for y in range(28):
     for x in range(28):
         for value in range(256):
             PositionedPixel.all_features[PositionedPixel.encode_feature(x, y, value)] \
                     = PositionedPixel(x, y, value)
+
+          
+#
+# Static variables for the NBFeature class
+#
+
+# a list of implemented feature types, extend when implementing new
+NBFeature.all_feature_classes = [PositionedPixel]
